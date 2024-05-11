e7db5a165bbb051b2ce08a49741a265440de0e17
sakhawat71 <sakhawat.showrav@gmail.com>
2024-05-12 01:59:57 +0600

    span to button

--- script.js
+++ script.js
@@ -34,72 +34,8 @@ function getInput(ticket){
     return Count;
 };
 
-// document.getElementById('bookButton').addEventListener('click',function(){
 
-
-// });
 function bookingConfirmation(){
     document.getElementById('form').style.display = "none";
 };
 
-// function ticketPrice(){
-//     const input = document.getElementById('first-input');
-//     const count = parseInt(input.value);
-//     const input2 = document.getElementById('eco-input');
-//     const count2 = parseInt(input2.value);
-//     // const count = getInput('first');
-//     // const count2 = getInput('eco');
-
-//     let subtotal = (count * 150) + (count2 * 100);   
-//     // subtotal = getInput('first') * 150 + getInput('eco') * 100;
-//     document.getElementById('subtotal').innerText = '$' + subtotal;
-
-//     let tax = subtotal * 0.1;
-//     document.getElementById('tax').innerText = '$' + tax;
-
-//     let total = subtotal + tax;
-//     document.getElementById('total').innerText = '$' + total;
-//     getInput();
-// };
-
-
-
-// function increaseDecrease2(isincrease){
-
-//     const Input = document.getElementById("ecoInput");
-//     const Count = parseInt(Input.value);
-//     let NewCount = 0;
-//     if( isincrease == true){
-//         NewCount = Count + 1;
-//     };
-//     if (isincrease == false && Count > 0){
-//         NewCount = Count - 1;
-//     };
-//     Input.value = NewCount;
-
-// };
-
-// document.getElementById("firstClassIncrease").addEventListener('click',function(){
-
-//     const firstClassInput = document.getElementById('firstClassInput');
-//     const firstClassCount = parseInt(firstClassInput.value);
-//     const firstClassNewCount = firstClassCount + 1;
-//     firstClassInput.value = firstClassNewCount;
-
-// });
-
-// document.getElementById("firstClassDecrease").addEventListener('click',function(){
-
-//     const firstClassInput = document.getElementById("firstClassInput");
-//     const firstClassCount = parseInt(firstClassInput.value);
-//     let firstClassNewCount = firstClassCount - 1;
-//     firstClassInput.value = firstClassNewCount;
-
-// });
-
-// document.getElementById("increase").addEventListener('click',function(){
-//     ;
-// });
-// document.getElementById("decrease").addEventListener('click',function(){
-//     increaseDecrease(false);
-// });
